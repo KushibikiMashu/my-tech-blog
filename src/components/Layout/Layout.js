937983872e91b7f1c8728e62757c4240a852efb8
KushibikiMashu <kushibiki3413@gmail.com>
2019-11-02 14:50:04 +0900

    TwitterのOGP画像のサイズを変更

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -31,7 +31,7 @@ const Layout = ({
         <meta name="description" content={description} />
         <meta property="og:site_name" content={title} />
         <meta property="og:image" content={metaImageUrl} />
-        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={metaImageUrl} />
