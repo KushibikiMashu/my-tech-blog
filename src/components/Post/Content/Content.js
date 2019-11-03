1ba2f5c23782c4c118d803cfa4613ba6486521c2
KushibikiMashu <kushibiki3413@gmail.com>
2019-11-03 23:28:49 +0900

    Happy Codingの文字をブログの最後に表示

--- src/components/Post/Content/Content.js
+++ src/components/Post/Content/Content.js
@@ -11,6 +11,9 @@ const Content = ({ body, title }: Props) => (
   <div className={styles['content']}>
     <h1 className={styles['content__title']}>{title}</h1>
     <div className={styles['content__body']} dangerouslySetInnerHTML={{ __html: body }} />
+    <div className={styles['content__body']}>
+      <p>Happy Coding 🎉</p>
+    </div>
   </div>
 );
 
