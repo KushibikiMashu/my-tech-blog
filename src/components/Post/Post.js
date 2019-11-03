8fbbd1e0bb369762570fe87faad52672653f8983
KushibikiMashu <kushibiki3413@gmail.com>
2019-11-03 11:26:23 +0900

    記事一覧を記事の下部にも表示

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -18,22 +18,26 @@ const Post = ({ post }: Props) => {
   const { tagSlugs, slug } = post.fields;
   const { tags, title, date } = post.frontmatter;
 
+  const postListLink = <Link className={styles['post__home-button']} to="/">記事一覧へ</Link>;
+
   return (
     <div className={styles['post']}>
-      <Link className={styles['post__home-button']} to="/">記事一覧へ</Link>
+
+      {postListLink}
 
       <div className={styles['post__content']}>
-        <Content body={html} title={title} />
+        <Content body={html} title={title}/>
       </div>
 
       <div className={styles['post__footer']}>
-        <Meta date={date} />
-        {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
-        <Author />
+        <Meta date={date}/>
+        {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs}/>}
+        <Author/>
+        {postListLink}
       </div>
 
       <div className={styles['post__comments']}>
-        <Comments postSlug={slug} postTitle={post.frontmatter.title} />
+        <Comments postSlug={slug} postTitle={post.frontmatter.title}/>
       </div>
     </div>
   );
