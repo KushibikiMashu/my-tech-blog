0b13d1fe57e7bca42f7ce6137463f4e8fb1f3575
KushibikiMashu <kushibiki3413@gmail.com>
2020-06-02 22:57:54 +0900

    公開日と更新日を表示するようにする

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -10,8 +10,12 @@ type Props = {
 
 const Meta = ({ date, updatedAt }: Props) => (
   <div className={styles['meta']}>
-    {updatedAt === undefined ? null : <p className={styles['meta__date']}>更新日: {moment(updatedAt).format('YYYY/MM/DD')}</p>}
-    <p className={styles['meta__date']}>公開日: {moment(date).format('YYYY/MM/DD')}</p>
+    <p className={styles['meta__date']}>
+      {updatedAt === undefined ? `公開日: ${moment(date).format('YYYY/MM/DD')}`
+        : `更新日: ${moment(updatedAt).format('YYYY/MM/DD')}`
+      }
+    </p>
+    <p className={styles['meta__date']}>by <a href="https://twitter.com/Panda_Program">@Panda_Program</a></p>
   </div>
 );
 
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -40,6 +40,7 @@ export const query = graphql`
         tags
         title
         socialImage
+        updatedAt
       }
     }
   }
