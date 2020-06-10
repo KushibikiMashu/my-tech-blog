b097d91c98191296f5facce18e32e6cbf12aca0c
KushibikiMashu <kushibiki3413@gmail.com>
2020-06-10 22:53:43 +0900

    test

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 // @flow strict
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Author from './Author';
 import Contacts from './Contacts';
 import Copyright from './Copyright';
@@ -13,6 +13,38 @@ type Props = {
   isIndex?: boolean,
 };
 
+const PopularPosts = () => {
+  // local
+  const [count, setCount] = useState({
+    '/nextjs-with-cms-esa': 73,
+    '/test-driven-development': 71,
+  })
+
+  // data
+  const endpoint = 'https://bookmark.hatenaapis.com/count/entry?url=https://panda-program.com/posts'
+  const paths= [
+    '/nextjs-with-cms-esa',
+    '/test-driven-development',
+  ]
+
+  useEffect(() => {
+    paths.forEach((path, index) => {
+      fetch(`${endpoint}${path}`).then(res => {
+          //   setCount({
+          //   ...count,
+          //   path: res.body,
+          // })
+          console.log(res)
+          console.log(res.json())
+        }
+      )
+    })
+  } , [])
+
+  return <>a</>
+}
+
+
 const Sidebar = ({ isIndex }: Props) => {
   const { author, copyright, menu } = useSiteMetadata();
 
@@ -21,6 +53,7 @@ const Sidebar = ({ isIndex }: Props) => {
       <div className={styles['sidebar__inner']}>
         <Author author={author} isIndex={isIndex} />
         <Menu menu={menu} />
+        <PopularPosts />
         <Category hasHeading={true} />
         <Contacts contacts={author.contacts} />
         <Copyright copyright={copyright} />
