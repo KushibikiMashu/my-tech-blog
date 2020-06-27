bf48ca7321c6b390fce9d69f117c35565fe38c73
KushibikiMashu <kushibiki3413@gmail.com>
2020-06-27 18:23:09 +0900

    シンタックスハイライトの色を変更

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,5 @@ require('./src/assets/scss/init.scss');
 
 // prism.js
 require('prismjs/themes/prism-tomorrow.css');
+require('prismjs/plugins/line-highlight/prism-line-highlight.css');
 require('./src/assets/prism/custom.scss');
