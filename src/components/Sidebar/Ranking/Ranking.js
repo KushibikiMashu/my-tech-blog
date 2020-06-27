c9416196fbcb09917036b87be047208b53bcc6c7
KushibikiMashu <kushibiki3413@gmail.com>
2020-06-27 15:52:46 +0900

    rankingにkeyを付与

--- src/components/Sidebar/Ranking/Ranking.js
+++ src/components/Sidebar/Ranking/Ranking.js
@@ -6,7 +6,7 @@ const Ranking = () => {
     {
       title: 'Next.js + esa.io + VercelでJAMStackな爆速ブログを構築する',
       path: '/posts/nextjs-with-cms-esa',
-      count: 74,
+      count: 75,
     },
     {
       title: 'テスト駆動開発（TDD）とは何か。コードで実践方法を解説します',
@@ -20,7 +20,7 @@ const Ranking = () => {
       <p style={{ margin: 0 }}>人気記事</p>
       <ul style={{ paddingLeft: '20px' }}>
         {posts.map((post) => (
-          <li>
+          <li key={post.title}>
             <Link to={post.path}>「{post.title}」(はてブ数 {post.count})</Link>
           </li>
         ))}
