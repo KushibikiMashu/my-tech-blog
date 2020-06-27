3bec37ba192b228a927fdb6b2b54542ba6dccb11
KushibikiMashu <kushibiki3413@gmail.com>
2020-06-27 23:33:49 +0900

    ブログの下書きを作成するコマンドを追加

---
+++ etc/post.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# コマンド
+# ./etc/post.sh
+
+# date書き換え
+now=$(date "+%Y-%m-%dT%H:%M:%S.000Z")
+img_template="/media/2020/0/00/2020_00_00__0.png"
+img=$(date "+/media/%Y/%m/%d/%Y_%m_%d__0.png")
+name=$(date "+%Y-%m-%d.md")
+
+# dateの書き換え
+# draftの書き換え
+# 画像パスの書き換え
+sed "3s/2020-00-00T00:00:00.000Z/$now/g" "./content/posts/template.md" \
+  | sed "6s/true/false/g" \
+  | sed "s|$img_template|$img|" > "./content/posts/2020/$name"
