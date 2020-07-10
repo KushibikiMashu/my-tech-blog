c4d474cd811482e322fd6b036de130e0709e3220
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-10 23:12:41 +0900

    feedで記事クリックをチェック

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,7 @@
 // @flow strict
 import React from 'react';
 import { Link } from 'gatsby';
+import { trackCustomEvent } from 'gatsby-plugin-google-analytics';
 import type { Edges } from '../../types';
 import styles from './Feed.module.scss';
 
@@ -16,12 +17,22 @@ const shortDescription = (description: ?string) => {
   return description.length >= 120 ? `${description.slice(0, 120)}...` : description;
 };
 
+const clickPostEvent = (title) => (e) => {
+  e.preventDefault();
+
+  trackCustomEvent({
+    category: 'post',
+    action: 'click',
+    label: title
+  });
+};
+
 const Feed = ({ edges }: Props) => (
   <div className={styles['feed']}>
     {edges.map((edge) => (
       <div className={styles['feed__item']} key={edge.node.fields.slug}>
         <h2 className={styles['feed__item-title']}>
-          <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug}>{edge.node.frontmatter.title}</Link>
+          <Link className={styles['feed__item-title-link']} to={edge.node.fields.slug} onClick={clickPostEvent(edge.node.frontmatter.title)}>{edge.node.frontmatter.title}</Link>
         </h2>
         <div className={styles['feed__item-meta']}>
           <span className={styles['feed__item-meta-time']}>公開日:{' '}</span>
@@ -31,14 +42,14 @@ const Feed = ({ edges }: Props) => (
           <span className={styles['feed__item-meta-divider']} />
           <span className={styles['feed__item-meta-category']}>
             <span className={styles['feed__item-meta-category-text']}>カテゴリ:{' '}</span>
-            <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']}>{edge.node.frontmatter.category}</Link>
+            <Link to={edge.node.fields.categorySlug} className={styles['feed__item-meta-category-link']} >{edge.node.frontmatter.category}</Link>
           </span>
         </div>
         <p className={styles['feed__item-description']}>
           {shortDescription(edge.node.frontmatter.description)}
           {'　'}
           <span className={styles['feed__item-readmore']}>
-            <Link to={edge.node.fields.slug}>
+            <Link to={edge.node.fields.slug} onClick={clickPostEvent(edge.node.frontmatter.title)}>
               記事を読む
             </Link>
           </span>
