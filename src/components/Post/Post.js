de997b5a54eae51ea0cb9b3273b1c0e71b9814ac
KushibikiMashu <kushibiki3413@gmail.com>
2020-07-29 22:00:58 +0900

    広告を追加

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -51,6 +51,18 @@ const Post = ({ post }: Props) => {
 
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs}/>}
 
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" />
+         {/* 横長 */}
+        <ins className="adsbygoogle"
+             style={{ display: 'block' }}
+             data-ad-client="ca-pub-4506236710956024"
+             data-ad-slot="2206907154"
+             data-ad-format="auto"
+             data-full-width-responsive="true" />
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+
         <div className={styles['post__footer-share']}>
           <div className={styles['post__footer-shareButtons']}>
             <ShareButtons shareTitle={shareTitle} url={postUrl}/>
