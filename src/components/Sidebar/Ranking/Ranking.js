85692995455b463774818d4729709c2d92b9349a
KushibikiMashu <kushibiki3413@gmail.com>
2020-08-04 23:23:23 +0900

    はてブからブックマーク数を取得

--- src/components/Sidebar/Ranking/Ranking.js
+++ src/components/Sidebar/Ranking/Ranking.js
@@ -6,22 +6,18 @@ const Ranking = () => {
     {
       title: 'Next.js + esa.io + VercelでJAMStackな爆速ブログを構築する',
       path: '/posts/nextjs-with-cms-esa',
-      count: 76,
     },
     {
       title: 'テスト駆動開発（TDD）とは何か。コードで実践方法を解説します',
       path: '/posts/test-driven-development',
-      count: 72,
     },
     {
       title: 'jQueryの作者による「毎日コードを書く」習慣の効果と自分で半年やってみた感想',
-      slug: '/posts/write-code-every-day',
-      count: 9,
+      path: '/posts/write-code-every-day',
     },
     {
       title: 'Next.js + TypeScriptにStorybookを導入して遭遇したエラーを全て共有します',
-      slug: '/posts/nextjs-storybook-typescript-errors',
-      count: 2,
+      path: '/posts/nextjs-storybook-typescript-errors',
     },
   ];
 
@@ -31,7 +27,12 @@ const Ranking = () => {
       <ul style={{ paddingLeft: '20px' }}>
         {posts.map((post) => (
           <li key={post.title}>
-            <Link to={post.path}>{post.title}(はてブ数 {post.count})</Link>
+            <Link to={post.path}>{post.title}</Link>
+            <div style={{ height: 13 }}>
+            <a href={`https://b.hatena.ne.jp/entry/https://panda-program.com${post.path}`}>
+              <img src={`https://b.hatena.ne.jp/entry/image/https://panda-program.com${post.path}`}/>
+            </a>
+            </div>
           </li>
         ))}
       </ul>
