f15a0de945bb20fc88c30aa3806076f2d1babe20
KushibikiMashu <kushibiki3413@gmail.com>
2021-01-02 22:43:25 +0900

    feat: zenn の記事を表示

--- src/components/Sidebar/Ranking/Ranking.js
+++ src/components/Sidebar/Ranking/Ranking.js
@@ -1,11 +1,9 @@
+// @flow strict
 import React from 'react';
 import Link from 'gatsby-link';
 import { useSiteMetadata } from '../../../hooks';
-
-type Post = {
-  title: string;
-  path: string;
-}
+import { posts, zennPosts } from './posts';
+import type { Post } from './posts';
 
 type RankingPost = {
   hatenaUrl: string;
@@ -13,29 +11,7 @@ type RankingPost = {
 } & Post
 
 const hatena = 'https://b.hatena.ne.jp/entry';
-
-const posts: Post[] = [
-  {
-    title: 'Next.jsのISRを使ってスプレッドシートをデータソースにして業務フローを変えた話',
-    path: '/posts/nextjs-isr-spreadsheet',
-  },
-  {
-    title: 'テスト駆動開発（TDD）とは何か。コードで実践方法を解説します',
-    path: '/posts/test-driven-development/',
-  },
-  {
-    title: 'Next.js + esa.io + VercelでJAMStackな爆速ブログを構築する',
-    path: '/posts/nextjs-with-cms-esa/',
-  },
-  {
-    title: '弁護士ドットコムライブラリーのフロントエンドのアーキテクチャ（Next.js + TypeScript）',
-    path: '/posts/bengo4com-library-frontend',
-  },
-  {
-    title: 'jQueryの作者による「毎日コードを書く」習慣の効果と自分で半年やってみた感想',
-    path: '/posts/write-code-every-day',
-  },
-];
+const zenn = 'https://zenn.dev';
 
 const getHatenaPosts = (posts: Post[], url: string): RankingPost[] => posts.map((post) => ({
   ...post,
@@ -43,13 +19,13 @@ const getHatenaPosts = (posts: Post[], url: string): RankingPost[] => posts.map(
   hatenaImgUrl: `${hatena}/image/${url}${post.path}`,
 }));
 
-const PostList = (props: { heading: string, posts: RankingPost[] }) => (
+const PostList = (props: { heading: string, posts: RankingPost[], host?: string }) => (
   <>
     <p style={{ margin: 0 }}>{props.heading}</p>
     <ul style={{ paddingLeft: '20px' }}>
       {props.posts.map((post) => (
         <li key={post.title}>
-          <Link to={post.path}>{post.title}</Link>
+          {props.host ? <a href={`${props.host}${post.path}`}>{post.title}</a> : <Link to={post.path}>{post.title}</Link> }
           <div style={{ height: 13 }}>
             <a href={post.hatenaUrl}>
               <img src={post.hatenaImgUrl} alt="はてなブックマーク数"/>
@@ -63,9 +39,11 @@ const PostList = (props: { heading: string, posts: RankingPost[] }) => (
 
 const Ranking = () => {
   const { url } = useSiteMetadata();
-  const sitePosts = getHatenaPosts(posts, url);
 
-  return <PostList heading="人気記事" posts={sitePosts} />;
+  return <>
+    <PostList heading="人気記事" posts={getHatenaPosts(posts, url)} />
+    <PostList heading="Zenn 投稿記事" posts={getHatenaPosts(zennPosts, zenn)} host={zenn} />
+  </>;
 };
 
 export default Ranking;
