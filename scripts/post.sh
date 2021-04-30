282cdb89be03f9d9004c6376f5b94072670324b9
KushibikiMashu <kushibiki3413@gmail.com>
2021-04-30 17:20:11 +0900

    feat: post.sh を更新

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,23 +1,21 @@
 #! /bin/bash
 
-# コマンド
-# ./etc/post.sh
-
 # date書き換え
+year=2021
 now=$(date "+%Y-%m-%dT%H:%M:%S.000Z")
-img_template="/media/2021/0/00/2021_00_00__0.png"
+img_template="/media/${year}/0/00/${year}_00_00__0.png"
 img=$(date "+/media/%Y/%m/%d/%Y_%m_%d__0.png")
 name=$(date "+%m/%Y-%m-%d.md")
+dir=$(date "+%m")
+
+mkdir -p "./content/posts/${year}/$dir"
 
 # dateの書き換え
 # draftの書き換え
 # 画像パスの書き換え
-sed "3s/2021-00-00T00:00:00.000Z/$now/g" "./content/posts/template.md" \
+sed "3s/${year}-00-00T00:00:00.000Z/$now/g" "./content/posts/template.md" \
   | sed "6s/true/false/g" \
-  | sed "s|$img_template|$img|" > "./content/posts/2021/$name"
+  | sed "s|$img_template|$img|" > "./content/posts/${year}/$name"
 
-dirname="$(date '+static/media/%Y/%m/%d')"
-
-if  [ ! -e "$dirname" ]; then
-  mkdir -p "$dirname"
-fi
+static_dirname="$(date '+static/media/%Y/%m/%d')"
+mkdir -p "$static_dirname"
